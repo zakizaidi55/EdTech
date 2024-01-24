c3dc15d8793f5a428a68c574a96cf1dca6e10e2b
zakizaidi55 <zakizaidi55@gmail.com>
2024-01-25 02:59:46 +0530

    user model

---
+++ EdTech/server/models/User.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+
+    lastName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+
+    password:{
+        type:String,
+        required:true,
+
+    },
+
+    accountType:{
+        type:String,
+        enum:["Admin", "Student", "Instructor"],
+    },
+
+    additionalDetails: {
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"Profile"
+    },
+
+    courses:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Profile"
+        }
+    ],
+
+    image: {
+        type:String,
+        required:true,
+    },
+
+    courseProgress:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"courseProgress",
+        }
+    ]
+});
+
+module.exports = mongoose.model("User", userSchema);
