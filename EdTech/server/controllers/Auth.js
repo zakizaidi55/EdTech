45682232c7d7283aea3673b64c7b7b02c3952b18
zakizaidi55 <zakizaidi55@gmail.com>
2024-02-05 18:26:15 +0530

    made changes in line 192 add account type in the payload

--- EdTech/server/controllers/Auth.js
+++ EdTech/server/controllers/Auth.js
@@ -189,7 +189,7 @@ exports.login = async(req, res) => {
             const payload = {
                 email:user.email,
                 id:user._id,
-                role:user.role,
+                accountType:user.accountType,
             }
             const token = jwt.sign(payload, process.env.JWT_SECRET, {
                 expiresIn:"2h "
