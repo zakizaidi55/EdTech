e3cb52ffb4d0cf940684c69c53ed94c966b9650e
zakizaidi55 <zakizaidi55@gmail.com>
2024-06-03 17:34:08 +0530

    changes in the index.js change https to http

--- EdTech/STUDYNOTION/server/index.js
+++ EdTech/STUDYNOTION/server/index.js
@@ -23,10 +23,10 @@ database.conncet();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin:"https://localhost:3000",
-        credentials:true,
-    })
+	cors({
+		origin:"http://localhost:3000",
+		credentials:true,
+	})
 )
 
 app.use(
