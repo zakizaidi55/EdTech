6dac8a1af1011d248a5f6fa03be2101e0faf6325
zakizaidi55 <zakizaidi55@gmail.com>
2024-06-05 19:48:54 +0530

    add error page

---
+++ EdTech/STUDYNOTION/src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div className='flex justify-center items-center text-3xl text-lg'>
+        Error-40 Not Found
+    </div>
+  )
+}
+
+export default Error
