ace0d89e132a4bb8d52e65aea9ee23076a57fd50
zakizaidi55 <zakizaidi55@gmail.com>
2024-06-14 21:43:13 +0530

    add form and dispatch function

--- EdTech/STUDYNOTION/src/pages/ForgotPassword.jsx
+++ EdTech/STUDYNOTION/src/pages/ForgotPassword.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import {getPasswordResetToken} from "../services/operations/authAPI"
 
 function ForgotPassword() {
     const {loading} = useSelector((state) =>state.auth);
 
     const [emailSent, setEmailSent] = useState(false);
     const [email, setEmail] = useState("");
+
+    const dispatch = useDispatch();
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        dispatch(getPasswordResetToken(email, setEmailSent));
+    }
+
   return (
-    <div>
+    <div className='text-white'>
         {
             loading ? (
                 <div className='flex justify-center items-center text-richblack-5 font-bold text-3xl'>
@@ -26,13 +37,41 @@ function ForgotPassword() {
                     <p>
                         {
                             !emailSent ? "Have no fear. We'll email you instructions to reset your password. If you dont have access to your email we can try account recovery" 
-                            : `We have sent the reset email to${email}`
+                            : `We have sent the reset email to ${email}`
                         }
                     </p>
 
-                    <form>
-                         
+                    <form onSubmit={handleOnSubmit}>
+                        {
+                            !emailSent && (
+                                <label>
+                                    <p>Email Address*</p>
+                                    <input
+                                        
+                                        required
+                                        type="email"
+                                        name="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        placeholder="Enter email address"
+                                        className="form-style w-full text-richblue-900"
+                                    />
+                                </label>
+                            )
+                        }
+
+                        <button type='submit'>
+                            {
+                                !emailSent ? "Reset password" : "Sent Email"
+                            }
+                        </button>
                     </form>
+
+                    <div>
+                        <Link to="/login">
+                            <p>Back to Login</p>
+                        </Link>
+                    </div>
 
                 </div>
             )
