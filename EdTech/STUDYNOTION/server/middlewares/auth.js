6b3e9f585ced7e43221009cdc8d4d464df7a090a
zakizaidi55 <zakizaidi55@gmail.com>
2024-08-18 18:59:03 +0530

    Enrolled page and My profile dropdown

--- EdTech/STUDYNOTION/server/middlewares/auth.js
+++ EdTech/STUDYNOTION/server/middlewares/auth.js
@@ -7,7 +7,7 @@ exports.auth = async(req, res, next) => {
     try {
         // extract token
         const token = req.cookies.token || req.body.token || req.header("Authorization").replace("Bearer ","");
-
+        
         if(!token) {
             return res.status(401).json({
                 success:false,
