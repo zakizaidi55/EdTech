2e78e6821ca77fe3ed2fcbc77c8ab919aab1393b
zakizaidi55 <zakizaidi55@gmail.com>
2024-08-20 17:59:33 +0530

    Side bar links and Edit profile pictures

--- EdTech/STUDYNOTION/src/components/core/Dashboard/Sidebarlinks.jsx
+++ EdTech/STUDYNOTION/src/components/core/Dashboard/Sidebarlinks.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import * as Icons from "react-icons/vsc"
 import { useDispatch } from 'react-redux';
 import { matchPath, NavLink, useLocation } from 'react-router-dom';
+import { resetCourseState } from '../../../reducers/slices/courseSlice';
 
 function Sidebarlinks({link, iconName}) {
     const Icon = Icons[iconName];
@@ -13,21 +14,26 @@ function Sidebarlinks({link, iconName}) {
     }
 
   return (
-    <div className='text-richblack-25'>
-      <NavLink to={link.path}
-      className={` relative px-8 py-2 text-sm font-medium${matchRoute(link.path) ? "bg-yellow-800" : "bg-opacity-0" }`}
-      >
-        <span className= {`absolute left-0 top-0 h-full w-[0.2rem] bg-yellow-50
-        ${matchRoute(link.path) ? "opacity-100" : "opacity-0"}`}>
-
-        </span>
-
-        <div className='flex items-center gap-x-2'>
-          <Icon className="text-lg"/>
-          <span>{link.name}</span>
-        </div>
-      </NavLink>
+    <NavLink
+    to={link.path}
+    onClick={() => dispatch(resetCourseState())}
+    className={`relative px-8 py-2 text-sm font-medium ${
+      matchRoute(link.path)
+        ? "bg-yellow-800 text-yellow-50"
+        : "bg-opacity-0 text-richblack-300"
+    } transition-all duration-200`}
+  >
+    <span
+      className={`absolute left-0 top-0 h-full w-[0.15rem] bg-yellow-50 ${
+        matchRoute(link.path) ? "opacity-100" : "opacity-0"
+      }`}
+    ></span>
+    <div className="flex items-center gap-x-2">
+      {/* Icon Goes Here */}
+      <Icon className="text-lg" />
+      <span>{link.name}</span>
     </div>
+  </NavLink>
   )
 }
 
